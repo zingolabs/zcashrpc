f1c491ab561f25405772ef1a7ada155712523ed6
Gygaxis Vainhardt <gygaxis@zingolabs.com>
2020-09-15 12:12:31 -0600

    Added generate functionality to zcash-rcli

--- zcash-rcli/src/commands.rs
+++ zcash-rcli/src/commands.rs
@@ -30,6 +30,12 @@ pub enum ZcashRcliCmd {
     #[options(help = "getinfo rpc call, no arguments.", name = "getinfo")]
     GetInfo(GetInfoCmd),
 
+    /// The 'generate' regtest command
+    #[options(
+        help = "generate rpc call. generate x (number of blocks to generate"
+    )]
+    Generate(GenerateCmd),
+
     /// The getblockchaininfo subcommand
     #[options(
         help = "getblockchaininfo rpc call, no arguments",
@@ -51,12 +57,18 @@ fn make_client(regtest: bool) -> zcashrpc::Client {
 }
 
 /// A simple Definition and Runnable implementation for commands that make an
-/// rpc call which takes no arguments
-macro_rules! zero_arg_run_impl {
-    ( $($command:ident, $rpc_call:ident),+) => {
+/// rpc call
+macro_rules! run_impl {
+    ( $($command:ident $rpc_call:ident $([$($arg:ident $of:ty)+])? ),+) => {
         $(///The $rpc_call rpc call
         #[derive(Command, Debug, abscissa_core::Options)]
         pub struct $command {
+            $($(
+                #[options(free)]
+                $arg: $of,
+            )+)?
+
+
             #[options(help = "command-specific help")]
             help: bool,
         }
@@ -64,7 +76,7 @@ macro_rules! zero_arg_run_impl {
             fn run(&self) {
                 abscissa_tokio::run(&$crate::application::APPLICATION, async {
                     let response =
-                        $crate::commands::make_client(true).$rpc_call();
+                        $crate::commands::make_client(true).$rpc_call($($(self.$arg),+)?);
                     println!("Help flag: {:?}", self.help);
                     println!("{:?}", response.await);
                 }).unwrap();
@@ -73,11 +85,10 @@ macro_rules! zero_arg_run_impl {
     };
 }
 
-zero_arg_run_impl!(
-    GetInfoCmd,
-    getinfo,
-    GetBlockchainInfoCmd,
-    getblockchaininfo
+run_impl!(
+    GetInfoCmd getinfo,
+    GetBlockchainInfoCmd getblockchaininfo,
+    GenerateCmd generate [how_many u32]
 );
 
 /// This trait allows you to define how application configuration is loaded.
