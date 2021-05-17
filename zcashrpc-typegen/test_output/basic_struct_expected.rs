b93e8c5559813ea2bec3eb56f919eea82c4c8eb4
zancas <zancas@zingolabs.org>
2021-05-16 18:33:14 -0600

    (re)moved zcashrpc-typegen into it's own workspace

--- zcashrpc-typegen/test_output/basic_struct_expected.rs
+++
@@ -1,9 +0,0 @@
-//procedurally generated response types, note that zcashrpc-typegen
-//is in early alpha, and output is subject to change at any time.
-pub mod basicstruct {
-    #[derive(Debug, serde :: Deserialize, serde :: Serialize)]
-    pub struct BasicstructResponse {
-        pub value_one: String,
-        pub value_two: bool,
-    }
-}
--- zcashrpc-typegen/test_output/vec_terminal_expected.rs
+++
@@ -1,8 +0,0 @@
-//procedurally generated response types, note that zcashrpc-typegen
-//is in early alpha, and output is subject to change at any time.
-pub mod VecOfNumber {
-    pub type VecOfNumberResponse = Vec<rust_decimal::Decimal>;
-}
-pub mod VecOfString {
-    pub type VecOfStringResponse = Vec<String>;
-}
