f7f2ccc3dbddc13956848042bf374d049af8de41
zancas <zancas@zingolabs.org>
2021-05-18 11:07:00 -0600

    remove (obsolete) typegen mod-and-fn

--- zcashrpc-macros/src/lib.rs
+++ zcashrpc-macros/src/lib.rs
@@ -1,6 +1,5 @@
 mod calls;
 mod cli_commands;
-mod responses;
 mod utils;
 
 use proc_macro::TokenStream;
@@ -26,11 +25,6 @@ pub fn declare_all_rpc_methods(_: TokenStream) -> TokenStream {
 #[proc_macro]
 pub fn declare_rpc_client_methods(input: TokenStream) -> TokenStream {
     utils::make_code(input.into(), calls::make_call).into()
-}
-
-#[proc_macro]
-pub fn declare_rpc_response_types(_input: TokenStream) -> TokenStream {
-    responses::declare_rpc_response_types().into()
 }
 
 #[proc_macro]
