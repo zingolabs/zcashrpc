51b45af6f709da3f0074b885fa4a9141c7782d54
zancas <zancas@zingolabs.org>
2021-05-25 13:58:01 -0600

    update method name and add stup unittest

--- zcashrpc-macros/src/typegen_interpreter.rs
+++ zcashrpc-macros/src/typegen_interpreter.rs
@@ -11,7 +11,7 @@ impl TemplateElementsBuilder {
             responses: self.responses.expect("Unininitialized??"),
         }
     }
-    fn check_response_or_args(&mut self, element: syn::Item) {
+    fn update_if_response_or_args(&mut self, element: syn::Item) {
         let id = unpack_ident_from_element(&element);
         if id.to_string().rfind("Response").is_some() {
             self.responses = Some(element);
@@ -65,7 +65,7 @@ fn unpack_ident_from_element(item: &syn::Item) -> &syn::Ident {
 }
 fn format_from_tg_to_rpc_client(
     rpc_name: String,
-    contents: Vec<syn::Item>,
+    mod_contents: Vec<syn::Item>,
 ) -> proc_macro2::TokenStream {
     //! Takes a typegen generated rpc definition, extracts elements:
     //!   rpc_name: Note the name is converted to a string, because the
@@ -79,8 +79,8 @@ fn format_from_tg_to_rpc_client(
         args: None,
         responses: None,
     };
-    for rpc_element in contents {
-        templatebuilder.check_response_or_args(rpc_element);
+    for rpc_element in mod_contents {
+        templatebuilder.update_if_response_or_args(rpc_element);
     }
     templatebuilder.build().populate_method_template()
 }
@@ -106,4 +106,15 @@ pub fn extract_response_idents() -> String {
     use std::io::Read as _;
     file.read_to_string(&mut src).expect("Unable to read file");
     src
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    mod format_from_tg_to_rpc_client {
+        use super::*;
+        #[ignore]
+        #[test]
+        fn getinfo_happy() {}
+    }
 }
